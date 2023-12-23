c4cfad5cb661860752abed17497739d5bb9135f8
Your Name <you@example.com>
2023-12-23 20:09:11 +0530

    React

---
+++ src/mod1.mjs
@@ -0,0 +1,5 @@
+import ui, {a,c,d} from './mod2.mjs'
+console.log(ui);
+console.log(a);
+console.log(c);
+console.log(d);
---
+++ src/mod2.mjs
@@ -0,0 +1,9 @@
+const a="Kiran";
+const b="Robin";
+const c="Farah";
+const d="Sourav";
+
+export default  b;
+export {a}
+export {c}
+export {d}
